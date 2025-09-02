37afc1c65fc9b87784f8333ecdee6caadc1d8b7b
LuSiEDa <wltlrtmdgus@naver.com>
2025-09-02 18:29:09 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.71 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -0,0 +1,11 @@
+def solution(n):
+    result =[]
+    for i in range(n):
+        row = []
+        for j in range(n):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        result.append(row)
+    return result
