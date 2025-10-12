1157ea4a5bfc915ca565f564252e1db772e41817
LuSiEDa <wltlrtmdgus@naver.com>
2025-10-12 16:38:51 +0900

    [Bronze V] Title: A-B, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        a, b = map(int, input().split())
+        if 0 < a < 10 and 0 < b < 10:
+            print(a-b)
+        else:
+            print("0 이상 10이하의 숫자를 입력해주세요.")
+    except EOFError:
+        break
+    except ValueError:
+        print("0 이상 10 이하의 숫자를 입력해주세요.")
+        
